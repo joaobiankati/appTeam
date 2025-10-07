ad3acc23c1971be1a1c9c999fc20a480115d00cd
joao <joaobiankati0164@gmail.com>
2025-10-06 22:42:49 -0300

    Loading

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components/native";
 import { Teams } from "./src/screens/Teams";
 import theme from "@theme/index";
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
 
 export default function App(){
+  const [fontsLoadead] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
   return(
     <ThemeProvider theme={theme}>
-      <Teams />
+      {!fontsLoadead ? <Teams /> : <Loading />}
     </ThemeProvider>
   )
 }
--- src/screens/Teams/styles.ts
+++ src/screens/Teams/styles.ts
@@ -2,17 +2,9 @@ import { View, Text, StyleSheet, Image} from "react-native"
 
 import styled from "styled-components/native"
 
-export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
-    },
-})
-
 export const Container = styled.View`
     flex: 1;
-    background-color: "#fff";
+    background-color: #fff;
     align-items: center;
     justify-content: center;
 
