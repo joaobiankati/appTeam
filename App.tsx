53d33f90cc3fd8d2e067726d7941a07b29c22318
joao <joaobiankati0164@gmail.com>
2025-10-06 21:54:04 -0300

    Theme

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
+import { ThemeProvider } from "styled-components";
 import { Teams } from "./src/screens/Teams";
+import theme from "@theme/index";
 
 export default function App(){
   return(
-    <Teams/>
+    <ThemeProvider theme={theme}>
+      <Teams />
+    </ThemeProvider>
   )
 }
--- src/screens/Teams/styles.ts
+++ src/screens/Teams/styles.ts
@@ -16,6 +16,7 @@ export const Container = styled.View`
     align-items: center;
     justify-content: center;
 
+    color: ${({theme}) => theme.COLORS.GRAY_100};
 `;
 
 export const Title = styled.Text``;
