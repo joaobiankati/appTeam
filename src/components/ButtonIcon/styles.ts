08cbd93c36941bb384bab226e71ae711e8eddb68
joao <joaobiankati0164@gmail.com>
2025-11-10 22:38:06 -0300

    Tag

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -1,20 +1,35 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons'
 
 export type ButtonIconTypeStyleProps = 'PRIMARY' | 'SECONDARY';
+export type ButtonIconSizeStyleProps = 'MD' | 'SM';
+export type ButtonIconBorderRadiusStyleProps = 'RIGHT' | 'FULL';
 
 type Props = {
   type: ButtonIconTypeStyleProps;
+  size: ButtonIconSizeStyleProps;
+  borderRadius: ButtonIconBorderRadiusStyleProps;
 }
 
 export const Container = styled(TouchableOpacity)<Props>`
   width: 54px;
   height: 54px;
+
+  ${({ size }) => size === 'SM' && css`
+    width: 44px;
+    height: 44px;
+  `};
+
+  border-radius: 8px;
+  ${({ borderRadius }) => borderRadius === 'RIGHT' && css`
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  `};
+
   justify-content: center;
   align-items: center;
   background-color: ${({ theme, type }) => type === 'PRIMARY' ? theme.COLORS.GREEN_500 : theme.COLORS.RED};
-  border-radius: 8px;
 `;
 
 export const Icon = styled(MaterialIcons).attrs(({ theme }) => ({
